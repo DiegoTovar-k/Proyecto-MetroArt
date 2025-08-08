c5396295a8e6acfb25a96ecf19c26718110878d3
PaoArteaga <paola.arteaga@correo.unimet.edu.ve>
2025-08-08 12:51:43 -0400

    main

---
+++ Museo.py
@@ -0,0 +1,12 @@
+
+class Museo:
+    def start(self):
+        
+        while True:
+            menu = input (""" Bienvenido a Metro Art. Seleccione la opción de su preferencia:
+1- Ver lista de obras por departamento
+2- Ver lista de obras por nacionalidad del autor
+3- Ver lista de obras por nombre del autor
+4- Mostrar detalles de la obra
+5- Salir
+""")
